c62fe3f60707dba4f46f1472319386ce8941875e
prakatesh <prakatesh2020@gmail.com>
2023-07-06 14:21:58 +0530

    home update

---
+++ src/component/Home.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Home() {
+  return (
+    <div>
+      <div className='homee'>
+        <div className='home-content'>
+            <h3>Hello,It's Me</h3>
+            
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default Home
