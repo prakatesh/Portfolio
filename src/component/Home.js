cd85277e0c9a13782b67ab1c9cc67346f1c56584
prakatesh <prakatesh2020@gmail.com>
2023-07-08 21:46:11 +0530

    animation in home page is set

--- src/component/Home.js
+++ src/component/Home.js
@@ -1,14 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 import '../asset/style/home.css'
 import image from '../asset/Image/pic-removebg-preview.png'
+import Typed from 'typed.js'
 function Home() {
+  const el = React.useRef(null)
+  React.useEffect(() => {
+    const typed = new Typed(el.current, {
+      strings: [`<h3>Hello,It's Me</h3><h1><span>Prakatesh</span> Balakrishnan</h1>`,`<h3>And I'm a <span style={{color:"#0ef"}}>Full Stack-Developer</span></h3>`],
+      typeSpeed: 100,
+      loop:true
+    });
+  });
+
   return (
     <div >
       <section className='home'>
         <div className='home-content'>
-            <h3>Hello,It's Me</h3>
-            <h1>Prakatesh Balakrishnan</h1>
-            <h3>And I'm a <span style={{color:"#0ef"}}>Full Stack-Developer</span></h3>
+            <p ref={el}></p>
             <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Aperiam, atque at. Delectus neque id quos itaque modi!</p>
             <div className='social-media' >
                 <a href="#"><box-icon type='logo' name='instagram'></box-icon></a>
